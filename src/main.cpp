13bd87cc19aa77c01c4b396fa35c0b9b87ac00d6
BXWBB <bxwbb@outlook.com>
2026-03-06 00:36:18 +0800

    v0.2.0 feat: 增加了变量的定义 增加了变量的再次修改 增加了if,elif,else语句 增加了代码块 增加了退出函数 增加了二元运算符+-*/ == > < >= <= != && || 增加了对数学表达式的支持，自动处理优先级和括号 增加了单行注释//和多行注释/* */ 完善了错误提示
    https://www.youtube.com/watch?v=vcSijrRsrY0感谢这位youtober的灵感和基础框架支持，虽然你已经两年没发视频了，请快点回归

---
+++ src/main.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <cstring>
+#include <filesystem>
+#include <fstream>
+#include <optional>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "./generation.h"
+
+namespace fs = std::filesystem;
+
+bool create_folder_in_current_dir(const std::string &folder_name, const bool overwrite = true) {
+    try {
+        const fs::path target_path = fs::current_path() / folder_name;
+        if (fs::exists(target_path)) {
+            if (fs::is_directory(target_path)) {
+                if (overwrite) {
+                    std::cout << "Folder already exists: " << target_path << ", skip creation\n";
+                    return true;
+                }
+                throw std::runtime_error("Folder already exists: " + target_path.string());
+            }
+            throw std::runtime_error(
+                "File with the same name exists, cannot create folder: " + target_path.string());
+        }
+        const bool created = fs::create_directories(target_path);
+        if (created) {
+            std::cout << "Folder created successfully: " << target_path << "\n";
+        }
+        return created;
+    } catch (const fs::filesystem_error &e) {
+        throw std::runtime_error("Failed to create folder: " + std::string(e.what()));
+    } catch (const std::exception &e) {
+        throw std::runtime_error("Failed to create folder: " + std::string(e.what()));
+    }
+}
+
+void create_basic_environment(const std::string &project_name) {
+    create_folder_in_current_dir(project_name);
+    std::fstream mcmeta(project_name + "/pack.mcmeta", std::ios::out);
+    mcmeta << "{\n"
+            << "\t\"pack\": {\n"
+            << "\t\t\"description\": \"Build by RedScript.\",\n"
+            << "\t\t\"pack_format\": 71,\n"
+            << "\t\t\"supported_formats\": [48, 81],\n"
+            << "\t\t\"min_format\": 48,\n"
+            << "\t\t\"max_format\": [88, 0]\n"
+            << "\t}\n"
+            << "}";
+    mcmeta.close();
+    create_folder_in_current_dir(project_name + "/data");
+    create_folder_in_current_dir(project_name + "/data/" + project_name);
+    create_folder_in_current_dir(project_name + "/data/" + project_name + "/function");
+    create_folder_in_current_dir(project_name + "/data/" + project_name + "/function/__util");
+    create_folder_in_current_dir(project_name + "/data/" + project_name + "/tags");
+    create_folder_in_current_dir(project_name + "/data/" + project_name + "/tags/function");
+    std::fstream get_var_func(project_name + "/data/" + project_name + "/function/__util/get_data.mcfunction",
+                              std::ios::out);
+    get_var_func << "$execute store result storage minecraft:__" << project_name <<
+            " $(ret) int 1 run data get storage minecraft:__" << project_name << " __stack[$(index)] 1";
+    get_var_func.close();
+    std::fstream tool_func_tag(project_name + "/data/" + project_name + "/tags/function/__util.json", std::ios::out);
+    tool_func_tag << "{\n"
+            << "\t\"values\": [\n"
+            << "\t\t\"test:__util/get_data\"\n"
+            << "\t]\n"
+            << "}";
+    tool_func_tag.close();
+    std::fstream all_func_tag(project_name + "/data/" + project_name + "/tags/function/__all.json", std::ios::out);
+    all_func_tag << "{\n"
+            << "\t\"values\": [\n"
+            << "\t\t\"#test:__util\",\n"
+            << "\t\t\"test:main\"\n"
+            << "\t]\n"
+            << "}";
+    all_func_tag.close();
+}
+
+std::string get_file_name(const std::string &path) {
+    const size_t sep_pos = path.find_last_of("/\\");
+    std::string filename_with_ext;
+    if (sep_pos == std::string::npos) {
+        filename_with_ext = path;
+    } else {
+        filename_with_ext = path.substr(sep_pos + 1);
+    }
+    const size_t dot_pos = filename_with_ext.find_last_of('.');
+    if (dot_pos == std::string::npos) {
+        return filename_with_ext;
+    }
+    return filename_with_ext.substr(0, dot_pos);
+}
+
+int main(const int argc, char *argv[]) {
+    if (argc == 2) {
+        if (std::strcmp(argv[1], "--version") == 0) {
+            std::cout << "Version: " << VERSION << std::endl;
+            std::cout << "Delta: nothing." << std::endl;
+        } else if (std::strcmp(argv[1], "-v") == 0) {
+            std::cout << "Version: " << VERSION << std::endl;
+        } else {
+            std::string contents;
+            {
+                std::stringstream contents_stream;
+                std::fstream input(argv[1], std::ios::in);
+                contents_stream << input.rdbuf();
+                contents = contents_stream.str();
+            }
+
+            Tokenizer tokenizer(contents);
+            std::vector<Token> tokens = tokenizer.tokenize();
+            Parser parser(tokens, argv[1]);
+            std::optional<NodeProg> prog = parser.parse_prog();
+
+            if (!prog.has_value()) {
+                print_error("Invalid program");
+            }
+
+            create_basic_environment(get_file_name(argv[1]));
+
+            Generator generator(prog.value(), get_file_name(argv[1]), get_file_name(argv[1]) + "/data/" + get_file_name(argv[1]) + "/function/");
+
+            generator.gen_prog();
+        }
+    } else {
+        std::cerr << "Usage: " << argv[0] << " [--version]" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
